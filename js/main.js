0213acb317ea33328d14b86b88d4b955277ac121
Agata Wisniewska <ag.e.wisniewska@gmail.com>
2016-02-18 21:37:39 +0100

    create div task

--- js/main.js
+++ js/main.js
@@ -2,24 +2,15 @@ $( document ).ready(function() {
     
     var dict = {};
   
-    
-    $('#datetimepicker1').datetimepicker();
+    $( "#datetimepicker1" ).datetimepicker();
 
 
     $('.mainbutton').click(function() { 
-    	// console.log("guzik") 
+    	
     	var task = $('#task').val() 
-    	// var date = $('#date_task').text()
-    	// console.log(task)
+    	
     	var date = $("#datetimepicker1").find("input").val();
-    	// console.log(date)
-    	// console.log(slownik)
     	
-    	// if (date in dict.keys().indexOf) {
-    	// dict[date] = task
-    	// dict[date] = task
-    	
-    	// dict[date].push(task);
        
     	if(Object.keys(dict).indexOf(date) < 0) {
     		dict[date] = new Array()
@@ -27,31 +18,19 @@ $( document ).ready(function() {
     	
     	dict[date].push(task);
     	
-        $('schedule').empty();
+        $('.schedule').empty();
     	
       
-        
         for (var i=0; i<=Object.keys(dict).length; i++) {
-    	   // $('#schedule p').append(Object.keys(dict)[i]); 
            var key = Object.keys(dict)[i];
-           // console.log(dict[klucz])
-           // $('.listoftasks p').append(dict[key]); 
-                // console.log(dict[date]);
-           
            $('.schedule').append(formatData(dict[key], key));
 
         }
-
-        
-                
- 
-        // console.log(key)
 
 	});
 
 
 });
-
 
 function formatData(tasks, date) {
     var resultTask = "";
@@ -59,6 +38,7 @@ function formatData(tasks, date) {
     for (var i=0; i<tasks.length; i++) {
         resultTask += ('<p>' + tasks[i] + '</p>')
     }
-    return("<div>" + resultDate + resultTask + '</div>')
+    var div = ("<div class='thumbnail tile tile-medium tile-teal'>" + resultDate + resultTask + '</div>')
+    return div
 }
 
